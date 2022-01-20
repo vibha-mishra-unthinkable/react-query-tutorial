71887a6041c1803a221ba6d39590b1c91a2e2671
vibha-mishra <vibhamish27@gmail.com>
2022-01-20 19:28:28 +0530

    added components,hooks,utils

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import "./App.css";
+
+import Home from "./components/Home";
+import RQSuperHeroes from "./components/RQSuperHeroes";
+import RQSuperHero from "./components/RQSuperHero";
+import SuperHeroes from "./components/SuperHeroes";
+import ParallelQueries from "./components/ParallelQueries";
+import DynamicQueries from "./components/DynamicQueries";
+import DependentQueries from "./components/DependentQueries";
+import PaginatedQueries from "./components/PaginatedQueries";
+import InfiniteQueries from "./components/InfiniteQueries";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/super-heroes">Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/super-heroes" element={<SuperHeroes />} />
+            <Route path="/rq-super-heroes/:heroId" element={<RQSuperHero />} />
+            <Route path="/rq-super-heroes" element={<RQSuperHeroes />} />
+            <Route path="/rq-parallel" element={<ParallelQueries />} />
+            <Route
+              path="/rq-dynamic-parallel"
+              element={<DynamicQueries heroIds={[1, 3]} />}
+            />
+            <Route
+              path="/rq-dependent"
+              element={<DependentQueries email="vishwas@example.com" />}
+            />
+            <Route path="/rq-paginated" element={<PaginatedQueries />} />
+            <Route path="/rq-infinite" element={<InfiniteQueries />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
   );
 }
 
---
+++ src/components/ParallelQueries.js
@@ -0,0 +1,18 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
+const fetchFriends = () => {
+  return axios.get("http://localhost:4000/friends");
+};
+
+const ParallelQueries = () => {
+  const { data: superHeroes } = useQuery("super-heroes", fetchSuperHeroes);
+  const { data: friends } = useQuery("friends", fetchFriends);
+  console.log(superHeroes, friends);
+  return <div>Parallel Queries</div>;
+};
+export default ParallelQueries;
